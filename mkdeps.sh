6670161aaa71d1db0dc133fbb3b17001213d9e03
Alan Zimmerman <alan.zimm@gmail.com>
2015-01-08 23:50:42 +0200

    Adding script to install all tweaked dependencies

---
+++ mkdeps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+(cd ../ghc-syb/utils                && cabal clean && cabal install --allow-newer)
+#(cd ../hslogger                     && cabal clean && cabal install --allow-newer)
+#(cd ~/tmp/hackage/djinn-ghc-0.0.2.2 && cabal clean && cabal install --allow-newer)
+#(cd ../../DanielG/ghc-mod           && cabal clean && cabal install --allow-newer)
+#(cd ../ghc-exactprint               && cabal clean && cabal install --allow-newer)
+
+cabal clean && cabal install   --allow-newer --enable-tests --dependencies-only
+cabal clean && cabal configure --allow-newer --enable-tests
+
