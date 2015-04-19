3a97f368bc510da477025a286ac1490afca41ba7
Matthew Pickering <matthewtpickering@gmail.com>
2015-04-19 22:47:12 +0100

    Add Preprocess module and support for LINE pragmas

---
+++ src/Language/Haskell/GHC/ExactPrint/Preprocess.hs
@@ -0,0 +1,45 @@
+module Language.Haskell.GHC.ExactPrint.Preprocess
+   (stripLinePragmas)
+
+   where
+
+import Data.List
+import Language.Haskell.GHC.ExactPrint.Types
+import Data.Maybe
+
+stripLinePragmas :: String -> (String, [Comment])
+stripLinePragmas = unlines' . unzip . findLines . lines
+  where
+    unlines' (a, b) = (unlines a, catMaybes b)
+
+findLines :: [String] -> [(String, Maybe Comment)]
+findLines = zipWith checkLine [1..]
+
+checkLine :: Int -> String -> (String, Maybe Comment)
+checkLine line s
+  | "{-# LINE" `isPrefixOf` s =
+      let (pragma, res) = getPragma s
+          size   = length pragma
+      in (res, Just $ Comment ((line, 1), (line, size+1)) pragma)
+  | otherwise = (s, Nothing)
+
+getPragma :: String -> (String, String)
+getPragma [] = error "Input must not be empty"
+getPragma s@(x:xs)
+  | "#-}" `isPrefixOf` s = ("#-}", "   " ++ drop 3 s)
+  | otherwise =
+      let (prag, remline) = getPragma xs
+      in (x:prag, ' ':remline)
+
+
+
+
+
+
+
+
+
+
+
+
+
