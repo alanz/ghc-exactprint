8be42c5b7c9a3347aaf9697006a78c5afc15e291
Matthew Pickering <matthewtpickering@gmail.com>
2015-06-22 11:00:38 +0100

    Add pattern substitution

--- TestTransform.hs
+++ TestTransform.hs
@@ -18,7 +18,8 @@ import Data.Generics.Schemes
 
 import HsExpr as GBC
 import HsBinds as GBC
-import HsSyn
+import HsSyn hiding (Pat)
+import qualified HsSyn as GHC (Pat)
 import SrcLoc
 import qualified SrcLoc as GHC
 import qualified RdrName as GHC
@@ -76,6 +77,8 @@ type Bind = HsBindLR  GHC.RdrName GHC.RdrName
 type Type = GHC.Located (GHC.HsType GHC.RdrName)
 
 type Decl = GHC.Located (GHC.HsDecl GHC.RdrName)
+
+type Pat = GHC.LPat GHC.RdrName
 
 mergeAnns :: Anns -> Anns -> Anns
 mergeAnns (a, b) (c,d) = (Map.union a c, Map.union b d)
@@ -143,8 +146,9 @@ runRefactoring :: Anns -> Module -> Refactoring GHC.SrcSpan -> (Anns, Module)
 runRefactoring as m r@Replace{}  =
   case rtype r of
     Expr -> replaceWorker as m parseExpr doExprReplacement r
---    Decl -> replaceWorker as m parseDecl r
+    Decl -> replaceWorker as m parseDecl doGenReplacement r
     Type -> replaceWorker as m parseType doGenReplacement r
+    Pattern -> replaceWorker as m parsePattern doGenReplacement r
 runRefactoring (as,sk) m ModifyComment{..} =
     ((Map.map go as, sk), m)
     where
@@ -183,6 +187,9 @@ findGen m ss = snd $ runState (doTrans m) (error (showGhc ss))
 findExpr :: Module -> SrcSpan -> Expr
 findExpr = findGen
 
+findPat :: Module -> SrcSpan -> Pat
+findPat = findGen
+
 findType :: Module -> SrcSpan -> Type
 findType = findGen
 
@@ -200,11 +207,15 @@ findLargestExpression ss e@(GHC.L l _) =
 
 -- Substitute variables into templates
 
-processSubsts :: Module -> (String, GHC.SrcSpan) -> (String, GHC.SrcSpan)
-processSubsts m (s, loc) = (s, loc)
-
 substTransform :: Data a => Module -> [(String, GHC.SrcSpan)] -> a -> M a
-substTransform m ss = everywhereM (mkM (exprSub m ss) `extM` typeSub m ss)
+substTransform m ss = everywhereM (mkM (exprSub m ss)
+                                    `extM` typeSub m ss
+                                    `extM` patSub m ss)
+
+patSub :: Module -> [(String, GHC.SrcSpan)] -> Pat -> M Pat
+patSub m subs old@(GHC.L l (VarPat name)) =
+  (resolveRdrName (findPat m) old subs name)
+patSub _ _ e = return e
 
 typeSub :: Module -> [(String, GHC.SrcSpan)] -> Type -> M Type
 typeSub m subs old@(GHC.L l (HsTyVar name)) =
@@ -423,13 +434,4 @@ setInitialDelta dp (mkAnnKey -> key) (as,sk) = (Map.adjust set key as, sk)
       let DP (c,l) = annEntryDelta
           newdelta = if c == 0 then DP (1, l) else annEntryDelta
       in a { annEntryDelta = newdelta}
-
-
-
-
-
-
-
-
-
 
