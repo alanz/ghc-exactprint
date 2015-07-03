9f846264a67ceafcb6b49aba1419dbe6e722bb3f
Alan Zimmerman <alan.zimm@gmail.com>
2015-07-03 22:01:52 +0200

    Update stack for current master and 7.10.2 rc2

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@
 ghc-config 7.11.20150621
 stack --skip-ghc-check --stack-yaml stack-7.11.yaml test
 
-ghc-config 7.10.1.20150619
+# ghc-config 7.10.1.20150619
+ghc-config 7.10.1.20150630
 stack test
