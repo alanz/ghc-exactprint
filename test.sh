77c8a9bea0e2a8eefa4e0338079ac1f762daad92
Alan Zimmerman <alan.zimm@gmail.com>
2015-08-20 15:53:01 +0200

    Rename stack.yaml to avoid conflict with ghc-mod

--- test.sh
+++ test.sh
@@ -10,4 +10,4 @@ stack --skip-ghc-check --stack-yaml stack-7.11.yaml test
 # ghc-config 7.10.1.20150630
 # ghc-config 7.10.1.20150715
 ghc-config 7.10.2
-stack test
+stack --stack-yaml stack-7.10.yaml test
