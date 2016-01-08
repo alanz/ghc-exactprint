1dd6a0631c8eb0e616e7a1d15dec01f60fd1115d
Alan Zimmerman <alan.zimm@gmail.com>
2016-01-08 22:46:17 +0200

    Can set up a mirror of hackage, with tabs
    Still need to clean up whitespace

--- tests/hackage/RoundTripHackage.hs
+++ tests/hackage/RoundTripHackage.hs
@@ -5,7 +5,40 @@ import qualified Data.Text as T
 
 main = do
   packages <- allCabalPackages
+  -- packages <- allCabalPackagesTest
   echo (T.pack $ show $ take 5 packages)
+  -- preparePackage "AAI"
+  mapM_ preparePackage packages
+
+-- ---------------------------------------------------------------------
+
+preparePackage :: Text -> IO ()
+preparePackage package = do
+  shell ("mkdir -p " <> workDir) empty
+  (ec,dir) <- shellStrict ("cabal get --destdir=" <> workDir <> " " <> package) empty
+  echo (T.pack $ "cabal get:" ++ show dir)
+  echo (T.pack $ show ec)
+  when (ec == ExitSuccess) $ do
+    let bits = T.splitOn " " (head $ T.lines dir)
+    echo (T.pack $ "cabal get:dir=" ++ show (last bits))
+    cleanPackage (last bits)
+  return ()
+
+-- ---------------------------------------------------------------------
+
+-- |Clean up whitespace in a package
+
+cleanPackage :: Text -> IO ()
+cleanPackage dir = do
+  echo ("still need to clean up:" <> dir)
+  -- return ()
+
+-- ---------------------------------------------------------------------
+
+allCabalPackagesTest :: IO [Text]
+allCabalPackagesTest
+  = return ["3d-graphics-examples","3dmodels","4Blocks","AAI","ABList"]
+
 
 allCabalPackages :: IO [Text]
 allCabalPackages = do
@@ -14,6 +47,8 @@ allCabalPackages = do
   let packages = T.lines r
   echo (T.pack $ show $ take 5 packages)
   return packages
+
+-- ---------------------------------------------------------------------
 
 workDir :: Text
 workDir = "./hackage-roundtrip-work"
