eb7feeb56979f3b84a6756a539fba4f9cc86c7d7
Alan Zimmerman <alan.zimm@gmail.com>
2017-05-04 22:23:52 +0200

    Extend test case for semis in if statement

--- tests/examples/ghc80/SemicolonIf.hs
+++ tests/examples/ghc80/SemicolonIf.hs
@@ -4,3 +4,12 @@ import Data.Text as Text
 
 replace :: Text -> Text
 replace = Text.map (\c -> if c == '_' then '.'; else c)
+
+replace1 :: Text -> Text
+replace1 = Text.map (\c -> if c == '_' ; then '.' else c)
+
+replace2 :: Text -> Text
+replace2 = Text.map (\c -> if c == '_'; then '.'; else c)
+
+replace4 :: Text -> Text
+replace4 = Text.map (\c -> if c == '_' then '.' else c)
