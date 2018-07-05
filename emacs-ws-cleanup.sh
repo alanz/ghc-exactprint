ecd350c9a673ddbd66f43a0e78dd560168768710
Alan Zimmerman <alan.zimm@gmail.com>
2018-07-05 22:29:56 +0200

    Make the file cleanup more robust

--- emacs-ws-cleanup.sh
+++ emacs-ws-cleanup.sh
@@ -3,10 +3,12 @@
 # Remove trailing whitespace, and replace tabs with the appropriate number of spaces
 
 for file in `find . -name "*.hs"`; do
-    echo "$file"; emacs --batch "$file" \
+    echo "$file"; emacs --batch -Q "$file" \
         --eval '(setq-default indent-tabs-mode nil)' \
         --eval '(whitespace-cleanup)' \
         --eval '(untabify (point-min) (point-max))' \
+        --eval '(perform-replace " " " " nil nil nil)' \
+        --eval '(perform-replace "" "" nil nil nil)' \
         -f 'save-buffer'
     fromdos $file
     # Make sure there is a newline at the end of the file
