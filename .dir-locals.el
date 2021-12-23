df0661769b8eeafcad0ed84cc7b7730e1f4ac1ad
Alan Zimmerman <alan.zimm@gmail.com>
2021-12-23 11:51:01 +0000

    Add emacs .dir-locals.el to ignore test files for HLS
    Ignore file for watching.
    
    Also ignores the hackage-roundrip-work file.

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+(
+ (nil
+  . ((eval . (progn
+               (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]hackage-roundtrip-work\\'")
+               (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]tests/examples\\'")))))
+)
