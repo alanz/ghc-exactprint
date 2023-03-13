f8ef8cc131df2be94d538ec78f40decc6bc9ee9b
Alan Zimmerman <alan.zimm@gmail.com>
2023-03-13 21:22:30 +0000

    Tweak CI

--- configure.sh
+++ configure.sh
@@ -164,4 +164,7 @@ rm -fr dist*
 # cabal configure -fdev -froundtrip --enable-tests --with-compiler=/opt/ghc/9.6.0.20230209/bin/ghc --allow-newer
 
 # cabal configure -fdev -froundtrip --enable-tests --with-compiler=/opt/ghc/9.6.0.20230214/bin/ghc --allow-newer
-cabal configure -fdev -froundtrip --enable-tests --with-compiler=/opt/ghc/9.6.1/bin/ghc
+
+# cabal configure -fdev -froundtrip --enable-tests --with-compiler=/opt/ghc/9.6.1/bin/ghc --allow-newer
+# cabal configure  --with-compiler=/opt/ghc/9.6.1/bin/ghc
+cabal configure -fdev  --enable-tests --with-compiler=/opt/ghc/9.6.1/bin/ghc
